3ecfd3c6a7a4fa876259614bac1e9796e0e4c74e
Sanjay361899 <sanjaythakur361899@gmail.com>
2022-12-22 17:46:47 +0530

    email list row left header of list done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+    <div className="app">
       <Header />
+      <div className="app__body">
       <SideBar />
+      <Routes>
+        <Route path="/mail" element={<Mail/>}/>
+        <Route path="/" element={<EmailList/>}/>
+      </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 };
 
